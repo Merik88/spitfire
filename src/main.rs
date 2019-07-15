bbe0235c3e5942305a95943825176cd960c7a1ae
Markus Eriksson <markus.eriksson@nepa.com>
2019-07-15 19:50:44 +0200

    Move a rectangle based on input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
+
+use quicksilver::{
+    Result,
+    geom::{Rectangle, Vector},
+    graphics::{Background, Color},
+    input::{GamepadButton, Key},
+    lifecycle::{State, Window, run}
+};
+ 
+struct Game {
+    player_position: Vector
+}
+ 
+impl State for Game {
+    fn new() -> Result<Game> {
+        Ok(Game {
+            player_position: Vector::new(375, 530)
+        })
+    }
+    
+    fn update(&mut self, window: &mut Window) -> Result<()> {
+        if window.keyboard()[Key::Right].is_down() ||
+            window.gamepads().iter().any(|pad| pad[GamepadButton::DpadRight].is_down())
+            {
+            self.player_position.x += 5.0;
+        }
+        if window.keyboard()[Key::Left].is_down() ||
+            window.gamepads().iter().any(|pad| pad[GamepadButton::DpadLeft].is_down())
+            {
+            self.player_position.x -= 5.0;
+        }
+        if window.keyboard()[Key::Up].is_down() ||
+            window.gamepads().iter().any(|pad| pad[GamepadButton::DpadUp].is_down())
+            {
+            self.player_position.y -= 5.0;
+        }
+        if window.keyboard()[Key::Down].is_down() ||
+            window.gamepads().iter().any(|pad| pad[GamepadButton::DpadDown].is_down())
+            {
+            self.player_position.y += 5.0;
+        }
+        Ok(())
+    }
+    
+    fn draw(&mut self, window: &mut Window) -> Result<()> {
+        window.clear(Color::BLUE)?;
+        window.draw(
+            &Rectangle::new(self.player_position,(50,60)),
+            Background::Col(Color::GREEN)
+        );
+        Ok(())
+    }
+}
+ 
 fn main() {
-    println!("Hello, world!");
+    run::<Game>("Spitfire", Vector::new(800, 600), Default::default());
 }
